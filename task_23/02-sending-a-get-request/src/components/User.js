91d95f4c183ccdb3cb11fb22b7f13267cdf75bbb
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-05-18 03:42:21 +0200

    add icons for gender - male and female

--- task_23/02-sending-a-get-request/src/components/User.js
+++ task_23/02-sending-a-get-request/src/components/User.js
@@ -7,8 +7,10 @@ const User = (props) => {
     <li className={classes.user}>
       <h2>{props.name}</h2>
       <h3>{props.email}</h3>
-      <p>{props.gender}</p>
       <p>{props.status}</p>
+      <p>
+        <img src={props.gender + '.png'} alt={props.gender} width="128" height="128"></img>
+      </p>
     </li>
   );
 };
